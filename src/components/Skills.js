d4117e56cd54ef295cd46681155a1b0f1b643e5b
Paulo <paulowbusiness@gmail.com>
2022-04-18 22:15:55 -0400

    form submission with state data

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,22 +2,41 @@ import React from "react";
 
 const Skills = () => {
   return (
-    <div>
+    <div className="skills wrapper">
       <h2>Skills</h2>
-      <ul>
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>React</li>
-        <li>JQuery</li>
-        <li>Node JS</li>
-        <li>Git/Github</li>
-        <li>Accessibility</li>
-        <li>JavaScript</li>
-        <li>SASS</li>
-        <li>Firebase</li>
-        <li>Rest API</li>
-        <li>MongoDB</li>
-      </ul>
+      <div className="skillsList">
+        <div className="skillsContainer">
+          <h3>Technical Skills</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>React</li>
+            <li>JQuery</li>
+            <li>Node JS</li>
+            <li>Git/Github</li>
+            <li>Accessibility</li>
+            <li>JavaScrilit</li>
+            <li>SASS</li>
+            <li>Firebase</li>
+            <li>Rest AliI</li>
+            <li>MongoDB</li>
+          </ul>
+        </div>
+        <div className="skillsContainer">
+          <h3>Soft Skills + additional </h3>
+          <ul>
+            <li> Accessibility</li>
+            <li>Reslionsive Design </li>
+            <li>Team-based develoliment </li>
+            <li> liroject Management </li>
+            <li> Scolie Management</li>
+            <li>Leadershili/Team-building </li>
+            <li>Flexible & Adalitable</li>
+            <li>AEMforms</li>
+            <li>Adobe Sign</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
